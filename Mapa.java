e5e306c6627366f75a45b2c50361d553b56c2f1d
Imanol Magaña <imanolmf2006@gmail.com>
2025-01-02 13:04:18 +0100

    Generar la posicion de enemigos. Tambien he cambiado el nombre de columnaTesoro a colTesoro. En el anterior commit se me olvido mencionar que puse todos los atributos.

--- Mapa.java
+++ Mapa.java
@@ -12,7 +12,7 @@ public class Mapa {
 
     //fila y columna random para el tesoro
     int filaTesoro = random.nextInt(6);
-    int columnaTesoro = random.nextInt(20);
+    int colTesoro = random.nextInt(20);
 
     //Atributos
     private char[][] tablero;
@@ -37,10 +37,26 @@ public class Mapa {
         tablero[filaJugador][0] = 'J';
 
         //generar posicion del tesoro sin mostrarlo en el tablero
-        posTesoro = new Posicion(filaTesoro, columnaTesoro);
+        posTesoro = new Posicion(filaTesoro, colTesoro);
 
         //generar posicion de enemigos
+        listadoEnemigos = new Posicion[3];
+        for (int i = 0; i < 3; i++) {
 
+            int filaEnemigo;
+            int colEnemigo;
+
+            do {
+                filaEnemigo = random.nextInt(6);
+                colEnemigo = random.nextInt(20);
+            }
+            //El while es para que donde se genere un enemigo no sea igual a un espacio vacio, es decir != ' '
+            while (tablero[filaEnemigo][colEnemigo] != ' ');
+
+            //Crear un objeto Posicion que representa la ubicacion de un enemigo en el tablero. Se almacena en listadoEnemigos, y puede ser 0, 1, o 2, (la i)
+            listadoEnemigos[i] = new Posicion(filaEnemigo, colEnemigo);
+            tablero[filaEnemigo][colEnemigo] = 'E';
+        }
 
     }
 }
