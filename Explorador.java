96e271d65eec86422917a3b8e27b15b8e28dd5ce
Imanol Magaña <imanolmf2006@gmail.com>
2025-01-02 11:28:18 +0100

    Añado Comentarios

--- Explorador.java
+++ Explorador.java
@@ -2,14 +2,18 @@ package Clases;
 
 import java.util.Random;
 
+//Clase explorador
 public class Explorador {
 
+    //Variables
     String nombre;
     Posicion posicionActual;
 
+    //filaAleatoria entre 0 y 100
     Random random = new Random();
     int filaAleatoria = random.nextInt(100);
 
+    //Constructor
     public Explorador(String nombre) {
         this.nombre = nombre;
 
