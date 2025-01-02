aacde3be48e7cc0611d63f6849d3c7908794e036
Imanol Magaña <imanolmf2006@gmail.com>
2025-01-02 11:23:42 +0100

    Constructor de Explorador.java y creacion de filaAleatoria de tipo int, que genera un numero aleatorio entre el 1 y el 100 para las coordenadas

--- Explorador.java
+++ Explorador.java
@@ -1,4 +1,18 @@
 package Clases;
 
+import java.util.Random;
+
 public class Explorador {
+
+    String nombre;
+    Posicion posicionActual;
+
+    Random random = new Random();
+    int filaAleatoria = random.nextInt(100);
+
+    public Explorador(String nombre) {
+        this.nombre = nombre;
+
+        this.posicionActual = new Posicion(filaAleatoria, 0);
+    }
 }
