23c7882f2bce11134c71bd1dfe05148f0c6782ec
Imanol Magaña <imanolmf2006@gmail.com>
2025-01-02 12:25:53 +0100

    Inicializar el tablero en el constructor

--- Mapa.java
+++ Mapa.java
@@ -1,4 +1,21 @@
 package Clases;
 
 public class Mapa {
+
+    private char[][] tablero;
+
+    public Mapa() {
+
+        // Inicializar el tablero. Se genera una columna de 20 espacios vacios"for j" 6 veces"por 6 filas for i"
+        tablero = new char[6][20];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 20; j++) {
+                tablero[i][j] = ' ';
+            }
+        }
+
+        //generar posicion del jugador
+
+
+    }
 }
