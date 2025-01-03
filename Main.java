e2b2fae7ffebee13d4c9ce4508022a8f40cc5afc
Imanol Magaña <imanolmf2006@gmail.com>
2025-01-03 13:22:48 +0100

    He hecho lo mismo de antes pero con el enemigo

--- Main.java
+++ Main.java
@@ -11,20 +11,30 @@ public class Main {
 
         //Bienvenida
         System.out.println(" *******************************************************");
-        System.out.println("Bienvenido al Juego del Explorador");
+        System.out.println("Bienvenido al Juego del explorador ");
         System.out.println(" *******************************************************");
 
         //Creo el Mapa
         Mapa mapa = new Mapa();
 
         //Creo el Explorador
-        System.out.println("Ingrese el nombre del Explorador: ");
+        System.out.println("Ingrese el nombre del explorador: ");
         String nombreExplorador = scanner.nextLine();
         Explorador explorador = new Explorador(nombreExplorador);
 
         //Iniciar el Jugador en el mapa
         Posicion posJugador = explorador.getPosicionActual();
         mapa.getTablero()[posJugador.getCoordenadaFila()][posJugador.getCoordenadaCol()] = 'J';
+
+        //Creo los enemigos
+        Enemigo[] enemigos = new Enemigo[3];
+
+        //Inicio los enemigos en el mapa
+        for (int i = 0; i < 3; i++) {
+            enemigos[i] = new Enemigo("Enemigo" + (i + 1));
+            Posicion posEnemigo = enemigos[i].getPosicionActual();
+            mapa.getTablero()[posEnemigo.getCoordenadaFila()][posEnemigo.getCoordenadaCol()] = 'E';
+        }
 
     }
 
